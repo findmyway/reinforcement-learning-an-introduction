eedecb901cfbd929221ee9cfc364c4c95fb73e6e
Tian Jun <tianjun@tianjun.me>
2016-12-12 13:05:23 +0800

    add mit license

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject rl "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"
+            :year 2016
+            :key "mit"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [philoskim/debux  "0.2.1"]
                  [incanter "1.5.7"]
